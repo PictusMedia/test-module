d1771ca18528f6e1fb1b3ebe9c5430de6f9d2ef8
IlyaEpifanov Work <ilya.digit@gmail.com>
2020-07-14 12:29:00 +0300

    gdfgh

--- Main/Resources/admin/views/partials/header/create.blade.php
+++
@@ -1,6 +0,0 @@
-<h3 class="d-flex w-100 font-weight-bold py-3 mb-4">
-    <div class="mr-auto">{{ $title }}</div>
-    <a href="{{ route($prefix . '.index') }}" type="button" class="btn btn-white rounded-pill d-block">
-        <span class="ion ion-ios-arrow-round-back"></span>&nbsp Вернуться
-    </a>
-</h3>
--- Main/Resources/admin/views/partials/meta.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="form-group">
-    <label class="form-label">Title:</label>
-    @input('meta[title]', isset($model->meta) ? $model->meta->title : false)
-</div>
-<div class="form-group">
-    <label class="form-label">Description:</label>
-    @textarea('meta[description]', isset($model->meta) ? $model->meta->description : false)
-</div>
